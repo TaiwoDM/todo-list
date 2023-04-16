0423d9b9104410fc3976c6b3ac733c81bc38c139
daniel <danielmayowataiwo@gmail.com>
2023-04-16 09:51:48 +0100

    implementing sessions after signup an signin  with jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,14 @@
 import User from "../models/User.js";
 
+import { promisify } from "util";
+import jwt from "jsonwebtoken";
+
+// User signup
 const signup = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
+    // if email or password is not provided
     if (!email || !password) {
       return res
         .status(400)
@@ -11,9 +16,22 @@ const signup = async (req, res, next) => {
     }
 
     const user = await User.create({ email, password });
+    user.password = undefined;
+
+    //  sign token and send token in cookie for session //
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+
+    res.cookie("jwt", token, {
+      expiresIn: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      ),
+    });
+    //  // // // // // // // // // // // // // // // // //
 
     return res.status(201).json({
-      message: "success",
+      status: "success",
       data: {
         user,
       },
@@ -23,6 +41,7 @@ const signup = async (req, res, next) => {
   }
 };
 
+// User signin
 const signin = async (req, res, next) => {
   try {
     const { email, password } = req.body;
@@ -52,8 +71,21 @@ const signin = async (req, res, next) => {
       });
     }
 
+    //x  sign token and send token in cookie for session //
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+
+    res.cookie("jwt", token, {
+      expiresIn: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      ),
+    });
+    //  // // // // // // // // // // // // // // // // //
+
     return res.status(200).json({
       status: "success",
+      data: { user },
     });
   } catch (err) {
     return next(err);
